07774aa34ea9b83844ebed2aecacff2a95f85963
bintang-dev <bintang@akarinti.tech>
2022-02-15 10:07:11 +0700

    feat: implement GlobalAdviceException

--- src/main/java/org/ait/project/aitboilerplate/modules/customer/exception/CustomerNotFoundException.java
+++ src/main/java/org/ait/project/aitboilerplate/modules/customer/exception/CustomerNotFoundException.java
@@ -3,8 +3,6 @@ package org.ait.project.aitboilerplate.modules.customer.exception;
 import org.ait.project.aitboilerplate.config.exception.ModuleException;
 import org.ait.project.aitboilerplate.shared.constant.enums.ResponseEnum;
 
-import java.util.function.Supplier;
-
 public class CustomerNotFoundException extends ModuleException {
 
     public CustomerNotFoundException(Object... argMessage) {
